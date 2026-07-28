92da3d8b991de70e97c5fd827041cb89773cf1d7
pallandos <commit@pallandos.dev>
2026-07-28 10:42:45 +0200

    refactor: change test to support go testing

---
+++ test/collectors/collectors_av_test.go
@@ -0,0 +1,20 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/pallandos/nano-zt/internal/collectors"
+)
+
+func TestCollectorsAV(t *testing.T) {
+	avhealth, err := collectors.GetGlobalAntivirusHealth()
+
+	if err != nil {
+		t.Errorf("Error: %s\n", err)
+	} else {
+		fmt.Printf("Is AV healthy : %t\n", avhealth.IsHealthy)
+		fmt.Printf("Status of AV : %s\n", avhealth.Status)
+		fmt.Printf("Description of AV : %s\n", avhealth.Description)
+	}
+}
